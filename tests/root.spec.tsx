556dd94b7ec84ce691e36f7eb04d21d52ff35018
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-12 00:10:40 +0300

    tests

--- tests/root.spec.tsx
+++ tests/root.spec.tsx
@@ -1,27 +1,48 @@
 import { test, expect } from '@playwright/test'
 
-test('Route navigation', async ({ page }) => {
-  // Go to the page
-  await page.goto('http://localhost:3000')
+test.describe('gaki no tsukai routes', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/en/gaki-no-tsukai')
+  })
 
-  // Click on the "episodes" link
-  await page.click('text="episodes"')
+  test('navigation', async ({ page }) => {
+    // Click on the "episodes" link
+    await page.click('text="Episodes"')
 
-  // Wait for the page to navigate
-  await page.waitForNavigation()
+    // Assert that the current URL is "/en/gaki-no-tsukai/episodes"
+    await page.waitForURL('/en/gaki-no-tsukai/episodes')
 
-  // Assert that the current URL is "/gaki-no-tsukai/episodes"
-  expect(page.url()).toBe('http://localhost:3000/gaki-no-tsukai/episodes')
+    // Go back to the main page
+    await page.goBack()
 
-  // Go back to the main page
-  await page.goBack()
+    // Click on the "series" link
+    await page.click('text="Series"')
 
-  // Click on the "series" link
-  await page.click('text="series"')
+    // Assert that the current URL is "/en/gaki-no-tsukai/series"
+    await page.waitForURL('/en/gaki-no-tsukai/series')
 
-  // Wait for the page to navigate
-  await page.waitForNavigation()
+    // Click on the "home" link
+    await page.click('text="Home"')
 
-  // Assert that the current URL is "/gaki-no-tsukai/series"
-  expect(page.url()).toBe('http://localhost:3000/gaki-no-tsukai/series')
+    // Assert that the current URL is "/en/gaki-no-tsukai"
+    await page.waitForURL('/en/gaki-no-tsukai')
+  })
+
+  test('localization', async ({ page }) => {
+    // Assert that the page is in English
+    await expect(page.getByText('Home')).toBeVisible()
+    await expect(page.getByText('Episodes')).toBeVisible()
+    await expect(page.getByText('Series')).toBeVisible()
+
+    // Change the language to Japanese
+    await page.click('text="JA"')
+
+    // Assert that the current URL is "/ja/gaki-no-tsukai"
+    await page.waitForURL('/ja/gaki-no-tsukai')
+
+    // Assert that the page is in Japanese
+    await expect(page.getByText('ホーム')).toBeVisible()
+    await expect(page.getByText('エピソード')).toBeVisible()
+    await expect(page.getByText('シリーズ')).toBeVisible()
+  })
 })
