00843f81740309bc3676be18c2731de814ca6b99
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-13 16:54:18 +0300

    fixed search param retaining, tests

--- src/app/ui/gaki-no-tsukai/header.tsx
+++ src/app/ui/gaki-no-tsukai/header.tsx
@@ -3,23 +3,26 @@
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
-import { resolveLanguage } from '@/lib/gaki-no-tsukai/utils'
 import LanguageSwitcher from '@/ui/gaki-no-tsukai/language-switcher'
 
 export default function Header() {
   const searchParams = useSearchParams()
-  const language = resolveLanguage(searchParams.get('lang'))
-  const params = new URLSearchParams()
-  params.set('lang', language.toLowerCase())
+  const language = searchParams.get('lang')
+
+  const createPageURL = (pathName: string) => {
+    if (!language) return pathName
+
+    const params = new URLSearchParams(searchParams)
+    params.set('lang', language.toLowerCase())
+    return `${pathName}?${params.toString()}`
+  }
 
   return (
     <header className="flex justify-between p-2 border-b border-white">
       <nav className="space-x-2">
-        <Link href={`/gaki-no-tsukai?${params.toString()}`}>Home</Link>
-        <Link href={`/gaki-no-tsukai/episodes?${params.toString()}`}>
-          Episodes
-        </Link>
-        <Link href={`/gaki-no-tsukai/series?${params.toString()}`}>Series</Link>
+        <Link href={createPageURL('/gaki-no-tsukai')}>Home</Link>
+        <Link href={createPageURL('/gaki-no-tsukai/episodes')}>Episodes</Link>
+        <Link href={createPageURL('/gaki-no-tsukai/series')}>Series</Link>
       </nav>
       <LanguageSwitcher />
     </header>
--- tests/root.spec.tsx
+++ tests/root.spec.tsx
@@ -1,16 +1,16 @@
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 
 test.describe('gaki no tsukai routes', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/en/gaki-no-tsukai')
+    await page.goto('/gaki-no-tsukai')
   })
 
   test('navigation', async ({ page }) => {
     // Click on the "episodes" link
     await page.click('text="Episodes"')
 
-    // Assert that the current URL is "/en/gaki-no-tsukai/episodes"
-    await page.waitForURL('/en/gaki-no-tsukai/episodes')
+    // Assert that the current URL is "/gaki-no-tsukai/episodes"
+    await page.waitForURL('/gaki-no-tsukai/episodes')
 
     // Go back to the main page
     await page.goBack()
@@ -18,31 +18,13 @@ test.describe('gaki no tsukai routes', () => {
     // Click on the "series" link
     await page.click('text="Series"')
 
-    // Assert that the current URL is "/en/gaki-no-tsukai/series"
-    await page.waitForURL('/en/gaki-no-tsukai/series')
+    // Assert that the current URL is "/gaki-no-tsukai/series"
+    await page.waitForURL('/gaki-no-tsukai/series')
 
     // Click on the "home" link
     await page.click('text="Home"')
 
-    // Assert that the current URL is "/en/gaki-no-tsukai"
-    await page.waitForURL('/en/gaki-no-tsukai')
-  })
-
-  test('localization', async ({ page }) => {
-    // Assert that the page is in English
-    await expect(page.getByText('Home')).toBeVisible()
-    await expect(page.getByText('Episodes')).toBeVisible()
-    await expect(page.getByText('Series')).toBeVisible()
-
-    // Change the language to Japanese
-    await page.click('text="JA"')
-
-    // Assert that the current URL is "/ja/gaki-no-tsukai"
-    await page.waitForURL('/ja/gaki-no-tsukai')
-
-    // Assert that the page is in Japanese
-    await expect(page.getByText('ホーム')).toBeVisible()
-    await expect(page.getByText('エピソード')).toBeVisible()
-    await expect(page.getByText('シリーズ')).toBeVisible()
+    // Assert that the current URL is "/gaki-no-tsukai"
+    await page.waitForURL('/gaki-no-tsukai')
   })
 })
