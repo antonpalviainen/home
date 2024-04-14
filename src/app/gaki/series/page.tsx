2ff95f34f6a390ef1d03532c50c438b82006a05d
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-15 01:56:01 +0300

    styling

--- src/app/gaki/series/page.tsx
+++ src/app/gaki/series/page.tsx
@@ -13,7 +13,7 @@ async function SeriesList({ language }: { language: Language }) {
   const series = await fetchSeries(language)
 
   return series.length ? (
-    <ul className="w-full border rounded-lg divide-y">
+    <ul className="w-full border rounded-lg divide-y dark:border-neutral-700 dark:divide-neutral-700">
       {series.map((series) => (
         <li key={series.id} className="px-2 py-1">
           <Link
