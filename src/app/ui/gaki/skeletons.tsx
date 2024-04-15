1d83faf62f9a292cfea706ddb911951ae04caf36
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-15 21:09:51 +0300

    styling

--- src/app/ui/gaki/skeletons.tsx
+++ src/app/ui/gaki/skeletons.tsx
@@ -1,17 +1,13 @@
-// Loading animation
-const shimmer =
-  'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent dark:before:via-white/5'
-
 export function HeaderSkeleton() {
   return (
-    <div className="flex justify-between px-3 py-2 border-b dark:border-white/10">
+    <div className="animate-pulse flex justify-between px-3 py-2 border-b dark:border-white/10">
       <div className="flex space-x-2">
-        <div className="inline-block w-16 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
-        <div className="inline-block w-16 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="inline-block w-16 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
+        <div className="inline-block w-16 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </div>
       <div className="flex space-x-2">
-        <div className="inline-block w-6 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
-        <div className="inline-block w-6 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="inline-block w-6 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
+        <div className="inline-block w-6 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </div>
     </div>
   )
@@ -19,12 +15,12 @@ export function HeaderSkeleton() {
 
 export function SeriesListItemSkeleton() {
   return (
-    <li className={`${shimmer} relative overflow-hidden px-2 py-1`}>
+    <li className="relative overflow-hidden px-2 py-1">
       <div className="flex justify-between space-x-2">
         {/* Name */}
-        <div className="w-3/4 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="w-3/4 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
         {/* Episodes */}
-        <div className="w-6 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="w-6 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </div>
     </li>
   )
@@ -32,7 +28,7 @@ export function SeriesListItemSkeleton() {
 
 export function SeriesListSkeleton() {
   return (
-    <ul className="w-full max-w-7xl border rounded-lg divide-y dark:border-white/10 dark:divide-white/10">
+    <ul className="animate-pulse w-full max-w-7xl border rounded-lg divide-y dark:border-white/10 dark:divide-white/10">
       {Array.from({ length: 50 }).map((_, index) => (
         <SeriesListItemSkeleton key={index} />
       ))}
@@ -45,15 +41,15 @@ export function TableRowSkeleton() {
     <tr>
       {/* Number */}
       <td className="px-2 py-1.5">
-        <div className="w-10 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="w-10 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </td>
       {/* Title */}
       <td className="py-1.5 w-full">
-        <div className="w-3/4 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="w-3/4 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </td>
       {/* Date */}
       <td className="px-2 py-1.5">
-        <div className="w-20 h-6 bg-neutral-100 rounded dark:bg-white/10"></div>
+        <div className="w-20 h-6 bg-neutral-100 rounded-lg dark:bg-white/10"></div>
       </td>
     </tr>
   )
@@ -62,7 +58,7 @@ export function TableRowSkeleton() {
 export function EpisodeTableSkeleton() {
   return (
     <div className="w-full max-w-7xl border rounded-lg dark:border-white/10">
-      <table className={`${shimmer} relative overflow-hidden`}>
+      <table className="animate-pulse relative overflow-hidden">
         <tbody className="divide-y dark:divide-white/10">
           {Array.from({ length: 50 }).map((_, index) => (
             <TableRowSkeleton key={index} />
