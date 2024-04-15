4a57a6f01d679878b4795b6b8416f205c6ad28a6
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-15 20:54:38 +0300

    tests

--- e2e/series.spec.tsx
+++ e2e/series.spec.tsx
@@ -1,7 +1,45 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
-test.describe('gaki no tsukai series route', () => {
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/gaki/series')
+})
+
+test.describe('episode list', () => {
+  test('is rendered', async ({ page }) => {
+    await expect(page.locator('ul')).toBeVisible()
+  })
+
+  test('contains series', async ({ page }) => {
+    await expect(
+      page.getByRole('link', { name: 'Batsu Game 22' })
+    ).toBeVisible()
+  })
+})
+
+test.describe('english toggle', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('/gaki/series')
+    await page.getByRole('link', { name: 'EN', exact: true }).click()
+  })
+
+  test('is working', async ({ page }) => {
+    await expect(page).toHaveURL('http://localhost:3000/gaki/series?lang=en')
+  })
+
+  test('series names are in english', async ({ page }) => {
+    await expect(page.getByRole('list')).toContainText('Batsu Game')
+  })
+})
+
+test.describe('japanese toggle', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('link', { name: 'JA', exact: true }).click()
+  })
+
+  test('is working', async ({ page }) => {
+    await expect(page).toHaveURL('http://localhost:3000/gaki/series?lang=ja')
+  })
+
+  test('series names are in Japanese', async ({ page }) => {
+    await expect(page.getByRole('list')).toContainText('罰ゲーム')
   })
 })
