8cc48678437bb078e513f2217c3f6ca85133d132
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-17 02:18:08 +0300

    progress select

--- src/app/ui/anime/progress-cell.tsx
+++ src/app/ui/anime/progress-cell.tsx
@@ -1,8 +1,42 @@
 'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import { decrementProgress, incrementProgress } from '@/lib/anime/actions'
+import { useClickOutside } from '@/lib/useClickOutside'
+
+function ProgressSelect({
+  progress,
+  episodes,
+}: {
+  progress: number | null
+  episodes: number
+}) {
+  const [open, setOpen] = useState(false)
+  const ref = useRef<HTMLSelectElement>(null)
+  useClickOutside(ref, () => setOpen(false))
+
+  return (
+    <>
+      {open ? (
+        <select ref={ref}>
+          {Array.from({ length: episodes + 1 }, (_, i) => (
+            <option key={i} value={i}>
+              {i}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <button
+          onClick={() => setOpen(true)}
+          className="hover:text-blue-600 dark:hover:text-blue-500"
+        >
+          {progress ?? '-'}
+        </button>
+      )}
+    </>
+  )
+}
 
 export function ProgressCell({
   id,
@@ -37,7 +71,8 @@ export function ProgressCell({
       >
         -
       </button>
-      {`${progress ?? '-'}/${episodes}`}
+      <ProgressSelect progress={progress} episodes={episodes} />
+      {`/${episodes}`}
       <button
         onClick={handleIncrement}
         className="w-5 h-5 ml-1 flex justify-center items-center border rounded-full hover:bg-neutral-100 hover:text-blue-600 dark:hover:text-blue-500"
