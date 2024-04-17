eed1ad25fa8dca231efa2883c20537456862f4a5
Anton Palviainen <anton.palviainen@outlook.com>
2024-04-17 05:10:46 +0300

    sort order function

--- src/app/lib/anime/data.ts
+++ src/app/lib/anime/data.ts
@@ -1,8 +1,58 @@
 import 'server-only'
 
+import { Prisma } from '@prisma/client'
+
 import prisma from '@/lib/prisma'
 
-export async function fetchFilteredAnime() {
+type OrderField =
+  | 'title'
+  | 'runtime'
+  | 'type'
+  | 'premiered'
+  | 'rating'
+  | 'progress'
+  | 'status'
+
+function generateSortOrder(
+  field: OrderField = 'status',
+  order: Prisma.SortOrder = 'asc'
+) {
+  const sortOrder =
+    order === 'asc' ? Prisma.SortOrder.asc : Prisma.SortOrder.desc
+  const fields = []
+
+  switch (field) {
+    case 'status':
+      fields.push({ status: sortOrder })
+      break
+    case 'runtime':
+      fields.push({ runtime: sortOrder })
+      break
+    case 'type':
+      fields.push({ type: sortOrder })
+      break
+    case 'premiered':
+      fields.push({ year: sortOrder }, { season: sortOrder })
+      break
+    case 'rating':
+      fields.push({ rating: sortOrder })
+      break
+    case 'progress':
+      fields.push({ progress: sortOrder })
+      break
+  }
+
+  fields.push({ title: sortOrder })
+
+  return fields
+}
+
+export async function fetchFilteredAnime(
+  orderField: OrderField = 'status',
+  orderDirection: Prisma.SortOrder = 'asc'
+) {
+  const orderBy = generateSortOrder(orderField, orderDirection)
+
   try {
     const data = await prisma.anime.findMany({
       select: {
@@ -17,13 +67,13 @@ export async function fetchFilteredAnime() {
         progress: true,
         status: true,
         studios: {
-          select: { id: true, name: true },
+          select: { name: true },
         },
         finishDates: {
           select: { date: true },
         },
       },
-      orderBy: [{ status: 'asc' }, { title: 'asc' }],
+      orderBy,
     })
 
     return data
