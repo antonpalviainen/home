e2e139bc7fa735f0627f18de65e7213c5961c301
Anton Palviainen <anton.palviainen@outlook.com>
2024-06-12 14:30:21 +0300

    pagination

--- src/app/ui/kpop/videos.tsx
+++ src/app/ui/kpop/videos.tsx
@@ -1,6 +1,8 @@
-import { fetchVideos } from '@/lib/kpop/data'
+import { fetchVideos, fetchVideosPages } from '@/lib/kpop/data'
 import type { Video } from '@/lib/kpop/definitions'
 import { formatDate, formatDuration } from '@/lib/utils'
+
+import Pagination from './pagination'
 
 function Tag({ name }: { name: string }) {
   return (
@@ -20,7 +22,7 @@ function Video({ video }: { video: Video }) {
             {formatDate(video.date)} - {video.channel.title} -{' '}
             {formatDuration(video.duration)}
           </span>
-          <div>
+          <div className="space-x-1">
             {video.tags?.map(({ name }) => (
               <Tag key={name} name={name} />
             ))}
@@ -41,6 +43,7 @@ export default async function Videos({
   order: 'asc' | 'desc'
 }) {
   const videos = await fetchVideos({ channel, page, order })
+  const totalPages = await fetchVideosPages({ channel })
 
   return (
     <main className="p-2 bg-neutral-200">
@@ -49,6 +52,9 @@ export default async function Videos({
           <Video key={video.id} video={video} />
         ))}
       </ul>
+      <div className="flex justify-center mt-2">
+        <Pagination totalPages={totalPages} />
+      </div>
     </main>
   )
 }
