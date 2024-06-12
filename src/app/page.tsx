d4674a0aef043d5c7751f89aa1a1fe2ef60620e4
Anton Palviainen <anton.palviainen@outlook.com>
2024-06-12 19:36:06 +0300

    index links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default async function Page() {
-  return <main>root</main>
+  return (
+    <main className="flex flex-col gap-4 p-4 text-3xl">
+      <Link href="/anime">anime</Link>
+      <Link href="/gaki">gaki no tsukai</Link>
+      <Link href="/kpop">kpop</Link>
+    </main>
+  )
 }
