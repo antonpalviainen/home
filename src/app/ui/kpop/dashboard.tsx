bae77d00b2e75dbbf1781e476f20b392738277f1
Anton Palviainen <anton.palviainen@outlook.com>
2024-06-14 16:43:01 +0300

    dashboard video card update

--- src/app/ui/kpop/dashboard.tsx
+++ src/app/ui/kpop/dashboard.tsx
@@ -1,43 +1,6 @@
-import Link from 'next/link'
-
 import { fetchNextVideo } from '@/lib/kpop/data'
-import type { Video } from '@/lib/kpop/definitions'
-import { formatDate, formatDuration } from '@/lib/utils'
 
-import Tag from './tag'
-
-function Video({ video }: { video: Video | null }) {
-  if (!video) {
-    return null
-  }
-
-  return (
-    <li className="mb-2">
-      <div className="p-2 space-y-1 bg-white rounded-lg shadow">
-        <Link
-          href={`https://www.youtube.com/watch?v=${video.id}`}
-          target="_blank"
-        >
-          {video.title}
-        </Link>
-        <div className="flex justify-between items-center">
-          <span className="text-neutral-500 text-sm">
-            {formatDate(video.date)} -{' '}
-            <Link href={`/kpop/${video.channel.name}`}>
-              {video.channel.title}
-            </Link>{' '}
-            - {formatDuration(video.duration)}
-          </span>
-          <div className="space-x-1">
-            {video.tags?.map(({ name }) => (
-              <Tag key={name} name={name} />
-            ))}
-          </div>
-        </div>
-      </div>
-    </li>
-  )
-}
+import Video from './video'
 
 export default async function Dashboard() {
   const channels = [
@@ -53,20 +16,28 @@ export default async function Dashboard() {
     'nmixx',
   ]
 
-  const videos = await Promise.all(
+  const results = await Promise.all(
     channels.map(async (channel) => {
       return await fetchNextVideo(channel)
     })
   )
 
-  if (!videos) {
+  if (!results.length) {
     return <p>No videos found</p>
   }
 
+  const videos = []
+
+  for (const result of results) {
+    if (result) {
+      videos.push(result)
+    }
+  }
+
   return (
-    <ul>
-      {videos.map((video, i) => (
-        <Video key={video?.id ?? i} video={video} />
+    <ul className="space-y-2">
+      {videos.map((video) => (
+        <Video key={video.id} video={video} />
       ))}
     </ul>
   )
